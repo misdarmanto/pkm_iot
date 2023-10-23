4646fb4165639c25fcea7f7eab0fc3ded9e12991
misdarmanto <misdarmanto1922@gmail.com>
2023-10-23 20:17:01 +0700

    init project

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Layout from "./layout";
+import Home from "./pages";
+import AddDevice from "./pages/addDevice";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/add-devices",
+        element: <AddDevice />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/addDevice.tsx
@@ -0,0 +1,5 @@
+const AddDevice = () => {
+  return <div>Add Device</div>;
+};
+
+export default AddDevice;
